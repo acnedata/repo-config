69f4cd666293cba115e3034419e2552ace8c6774
Iven Winkelmann <winkelmann.iven@gmail.com>
2022-10-27 15:22:15 +0200

    inital version of script

--- install.sh
+++ install.sh
@@ -4,12 +4,6 @@ PYTHON_VERSION="3.9"
 
 github="https://raw.githubusercontent.com/acnedata/repo-config/main"
 
-download () { 
-    curl --silent --create-dirs --location "${github}/${1}" -o $1
-    envsubst < $1
-    }
-
-
 declare -a config_files=(
     ".pre-commit-config.yaml"
     ".github/workflows/pre-commit.yml"
@@ -17,9 +11,21 @@ declare -a config_files=(
     ".devcontainer/Dockerfile"
 )
 
-echo "Installed:"
 
-for cf in ${config_files[@]}; do
-    download $cf
-    echo "- ${cf}"
-done
+mkfile () { mkdir -p "$(dirname "$1")" && touch "$1" ;  }
+
+download () { curl --silent --create-dirs --location "${github}/${1}"; }
+
+main () {
+    echo "Installed:"
+    export PYTHON_VERSION
+
+    for cf in ${config_files[@]}; do
+        download $cf | envsubst | mkfile $cf
+        echo "- ${cf}"
+    done
+
+    unset PYTHON_VERSION
+}
+
+main
