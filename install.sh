1c61123cd2f5479f2418d2d4c9530c2ad5ed060d
Iven Winkelmann <winkelmann.iven@gmail.com>
2022-10-27 14:15:27 +0200

    added github action

--- install.sh
+++ install.sh
@@ -1,8 +1,17 @@
 #bin/sh
 
-alias download="curl --location --remote-name"
+download () { curl --silent --location $1; }
 
 github="https://raw.githubusercontent.com/acnedata/repo-config/main"
 
-download "${github}/.pre-commit-config.yaml"
+declare -a config_files=(
+    ".pre-commit-config.yaml"
+    ".github/workflows.pre-commit.yml"
+)
 
+echo "Installed:"
+
+for cf in ${config_files[@]}; do
+    download "${github}/${cf} ${cf}"
+    echo "- ${cf}"
+done
