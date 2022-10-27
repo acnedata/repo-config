cb00284be8cca42e3b6db4d3791b1bb1c3fe0b9e
Iven Winkelmann <winkelmann.iven@gmail.com>
2022-10-27 13:50:40 +0200

    fixed github var

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #bin/sh
 
 alias download="curl --location --remote-name"
-alias github="https://raw.githubusercontent.com/acnedata/repo-config/main"
+
+github="https://raw.githubusercontent.com/acnedata/repo-config/main"
 
 download "${github}/.pre-commit-config.yaml"
 
