f6f701afb9e713c18ec6491876f0cf6732b5fc6a
Iven Winkelmann <winkelmann.iven@gmail.com>
2022-10-27 14:57:51 +0200

    template test

--- install.sh
+++ install.sh
@@ -1,17 +1,25 @@
 #bin/sh
 
-download () { curl --silent --location $1; }
+PYTHON_VERSION="3.9"
 
 github="https://raw.githubusercontent.com/acnedata/repo-config/main"
+
+download () { 
+    curl --silent --create-dirs --location "${github}/${1}" -o $1
+    envsubst < $1
+    }
+
 
 declare -a config_files=(
     ".pre-commit-config.yaml"
     ".github/workflows/pre-commit.yml"
+    ".devcontainer/devcontainer.json"
+    ".devcontainer/Dockerfile"
 )
 
 echo "Installed:"
 
 for cf in ${config_files[@]}; do
-    download "${github}/${cf} ${cf}"
+    download $cf
     echo "- ${cf}"
 done
