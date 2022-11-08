0abc528d6e7a29c0c6875f713869973c8471e6a1
Iven Winkelmann <winkelmann.iven@gmail.com>
2022-11-08 10:43:28 +0100

    added .gitignore

--- install.sh
+++ install.sh
@@ -5,24 +5,27 @@ PYTHON_VERSION="3.9"
 
 # Declare the files to be installed here
 declare -a config_files=(
+    ".gitignore"
     ".pre-commit-config.yaml"
     ".github/workflows/pre-commit.yml"
     ".devcontainer/devcontainer.json"
     ".devcontainer/Dockerfile"
 )
 
+# location of config files
 github="https://raw.githubusercontent.com/acnedata/repo-config/main/configs"
 
 
+# Main script
 mkfile () { mkdir -p "$(dirname "$1")" && cat - > $1;  }
 
 download () { curl --silent --create-dirs --location "${github}/${1}"; }
 
+# Fill in the python version in the config files
 main () {
     echo "Installed:"
     export PYTHON_VERSION
     export PYTHON_VERSION_DOTLESS="${PYTHON_VERSION//.}"
-    
 
     for cf in ${config_files[@]}; do
         download $cf | envsubst '${PYTHON_VERSION}, ${PYTHON_VERSION_DOTLESS}' | mkfile $cf
